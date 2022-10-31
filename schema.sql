6fce48836858a307b0a292ff447e705425c03eed
lucabenaventew <cacachuuuluca@gmail.com>
2022-10-31 13:37:44 -0500

    Improve execution time

--- schema.sql
+++ schema.sql
@@ -1,57 +1,55 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
+CREATE TABLE owners(
   id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(100),
+  full_name VARCHAR(20),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
   weight_kg DECIMAL,
-  PRIMARY KEY (id)
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD COLUMN species VARCHAR(50);
-
--- 3
-CREATE TABLE owners (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    full_name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    PRIMARY KEY(id)
-);
-CREATE TABLE species (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    PRIMARY KEY(id)
-);
-ALTER TABLE animals ADD PRIMARY KEY(id);
-ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD species_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(id);
-ALTER TABLE animals ADD owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_owner_id FOREIGN KEY(owner_id) REFERENCES owners(id);
-
--- 4
-CREATE TABLE vets (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    date_of_graduation DATE NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE specializations (
-    vet_id INT NOT NULL,
-    species_id INT NOT NULL,
-    PRIMARY KEY (vet_id, species_id),
-    CONSTRAINT FK_vets FOREIGN KEY (vet_id) REFERENCES vets (id),
-    CONSTRAINT FK_species FOREIGN KEY (species_id) REFERENCES species (id)
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE visits (
-    animal_id INT NOT NULL,
-    vet_id INT NOT NULL,
-    visit_date DATE NOT NULL,
-    CONSTRAINT fk_animal_id FOREIGN KEY(animal_id) REFERENCES animals(id),
-    CONSTRAINT fk_vets_id FOREIGN KEY(vet_id) REFERENCES vets(id)
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
+-- (Pair-programming)
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX animal_index ON visits(animal_id);
+CREATE INDEX vet_index ON visits(vet_id);
+CREATE INDEX email_index ON owners(email);
